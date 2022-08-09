70eff60ad15c746300f42010ce108654cbb37f91
VNavrockij <vitalik.nav@gmail.com>
2022-08-09 16:51:47 +0300

    Initial Commit

---
+++ Balls.playground/Contents.swift
@@ -0,0 +1,11 @@
+import PlaygroundSupport
+import UIKit
+
+var greeting = " Hello "
+
+public class Balls: UIView {
+    // color list
+//    private var colors: [UIColor]
+    // balls
+//    private var balls: [UIView] = []
+}
