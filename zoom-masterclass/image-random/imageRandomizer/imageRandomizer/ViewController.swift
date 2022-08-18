b76fb43029e7a70251985db4103c77f665d8911b
VNavrockij <vitalik.nav@gmail.com>
2022-08-18 16:41:03 +0300

    add: outlet and action

--- zoom-masterclass/image-random/imageRandomizer/imageRandomizer/ViewController.swift
+++ zoom-masterclass/image-random/imageRandomizer/imageRandomizer/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageNameLbl: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchRandomImgBtn(_ sender: UIButton) {
+        
+    }
+    
 }
 
