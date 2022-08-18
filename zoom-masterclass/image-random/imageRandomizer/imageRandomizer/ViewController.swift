5c517c390532c67ae95b7de13e45726ec892bf1b
VNavrockij <vitalik.nav@gmail.com>
2022-08-18 16:55:44 +0300

    add: random reload img and label

--- zoom-masterclass/image-random/imageRandomizer/imageRandomizer/ViewController.swift
+++ zoom-masterclass/image-random/imageRandomizer/imageRandomizer/ViewController.swift
@@ -11,14 +11,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageNameLbl: UILabel!
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var myButton: UIButton!
+    
+    let words = ["Pretty", "Lovely", "Great", "Awesome", "Nice"]
+    let imgNames = ["1", "2", "3", "4", "5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
     @IBAction func touchRandomImgBtn(_ sender: UIButton) {
+        updateUI()
         
+    }
+    
+    func updateUI() {
+        imageNameLbl.text = words.randomElement()
+        image.image = UIImage(named: imgNames.randomElement()!)
     }
     
 }
